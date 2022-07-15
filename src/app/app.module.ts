439c797da6f1b1ebca036c5963a337e5c4dea277
LasseTimmermanns <Lasse.mainkonto@gmail.com>
2022-07-15 15:26:54 +0200

    Created first Components and Implemented Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ExploreComponent } from './explore/explore.component';
+import { CardComponent } from './explore/card/card.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const allRoutes: Routes = [
+  {path: "", component: ExploreComponent},
+  {path: "explore", component: ExploreComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ExploreComponent,
+    CardComponent
   ],
   imports: [
+    RouterModule.forRoot(allRoutes),
     BrowserModule,
     AppRoutingModule
   ],
